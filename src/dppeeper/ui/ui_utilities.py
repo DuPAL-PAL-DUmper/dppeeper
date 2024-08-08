b78a97e291a7c77f4260bed8d6ced56c772ddcfa
Fabio Battaglia <tabaglio@posteo.net>
2024-08-08 18:34:14 +0200

    Test grid position generation for SIP packages

--- src/dppeeper/ui/ui_utilities.py
+++ src/dppeeper/ui/ui_utilities.py
@@ -17,28 +17,35 @@ class UIUtilities:
                 top_bottom: list[int] = [val for (idx, val) in enumerate(pins_per_side) if (idx % 2) == 1]
                 left_right: list[int] = [val for (idx, val) in enumerate(pins_per_side) if (idx % 2) == 0]
                 return (max(top_bottom) + 6, max(left_right) + 6)
+            case _:
+                raise ValueError(f'Number of sides {len(pins_per_side)} is not supported')
             
-        def calculatePinPosition(pin_no: int, isLabel: bool, pins_per_side: list[int], grid_size: Tuple[int, int]) -> Tuple[int, int]:
-            match len(pins_per_side):
-                case 1:
+    def calculatePinPosition(pin_no: int, isLabel: bool, pins_per_side: list[int]) -> Tuple[int, int]:
+        grid_size: list[int] = UIUtilities.calculateGridSize(pins_per_side)
+
+        if pin_no > sum(pins_per_side):
+            raise ValueError(f'Pin structure does not fit pin number {pin_no}')
+
+        match len(pins_per_side):
+            case 1:
                     return (1 + (0 if isLabel else 1), pin_no)
-                case 2:
-                    height: int = grid_size[1]
+            case 2:
+                height: int = grid_size[1]
                     
-                    if pin_no > pins_per_side[0]: # Right side
-                        pos: int = (height - 1) - (pin_no - pins_per_side[0]) 
-                        return (5 + (1 if isLabel else 0), pos)
-                    else: # Left side
-                        return (2 - (1 if isLabel else 0), pin_no)
-                case 4:
-                    if pin_no > pins_per_side[0] + pins_per_side[1] + pins_per_side[2]: # Top side
-                        top_pin_no: int = pin_no - pins_per_side[0] - pins_per_side[1] - pins_per_side[2]
-                        return ((grid_size[0] - 3) - top_pin_no, 2 - (1 if isLabel else 0))
-                    elif pin_no > pins_per_side[0] + pins_per_side[1]: # Right side
-                        right_pin_no: int = pin_no - pins_per_side[0] - pins_per_side[1]
-                        return (grid_size[0] - 3 + (1 if isLabel else 0), (grid_size[1] - 3) - right_pin_no)
-                    elif pin_no > pins_per_side[0]: # Bottom side
-                        bottom_pin_no: int = pin_no - pins_per_side[0]
-                        return (3 + bottom_pin_no, pins_per_side[0] + 2 + (1 if isLabel else 0))
-                    else: # Left side
-                        return (1 + (0 if isLabel else 1), 3 + pin_no)
+                if pin_no > pins_per_side[0]: # Right side
+                    pos: int = (height - 1) - (pin_no - pins_per_side[0]) 
+                    return (5 + (1 if isLabel else 0), pos)
+                else: # Left side
+                    return (2 - (1 if isLabel else 0), pin_no)
+            case 4:
+                if pin_no > pins_per_side[0] + pins_per_side[1] + pins_per_side[2]: # Top side
+                    top_pin_no: int = pin_no - pins_per_side[0] - pins_per_side[1] - pins_per_side[2]
+                    return ((grid_size[0] - 3) - top_pin_no, 2 - (1 if isLabel else 0))
+                elif pin_no > pins_per_side[0] + pins_per_side[1]: # Right side
+                    right_pin_no: int = pin_no - pins_per_side[0] - pins_per_side[1]
+                    return (grid_size[0] - 3 + (1 if isLabel else 0), (grid_size[1] - 3) - right_pin_no)
+                elif pin_no > pins_per_side[0]: # Bottom side
+                    bottom_pin_no: int = pin_no - pins_per_side[0]
+                    return (3 + bottom_pin_no, pins_per_side[0] + 2 + (1 if isLabel else 0))
+                else: # Left side
+                    return (1 + (0 if isLabel else 1), 3 + pin_no)
--- tests/test_ui_utilities.py
+++ tests/test_ui_utilities.py
@@ -5,15 +5,29 @@
 import sys
 from typing import Type
 
+import pytest
+
 from dppeeper.ui.ui_utilities import UIUtilities
 
 sys.path.insert(0, './src') # Make VSCode happy...
 
-def test_DIP_Grid_calculation():
+def test_grid_size_calculation():
     # SIP 12 pin IC
     assert UIUtilities.calculateGridSize([12]) == (4, 14)
     # DIP 20 pin IC (left and right pin count)
     assert UIUtilities.calculateGridSize([10, 10]) == (8, 12)
     # QUAD 48 pin (Left 14 + Bottom 10 + Right 14 + Top 10)
     assert UIUtilities.calculateGridSize([14, 10, 14, 10]) == (16, 20)
+
+def test_SIP_grid_position_calculation():
+    assert UIUtilities.calculatePinPosition(pin_no = 1, isLabel = False, pins_per_side = [10]) == (2, 1)
+    assert UIUtilities.calculatePinPosition(pin_no = 1, isLabel = True, pins_per_side = [10]) == (1, 1)
+    assert UIUtilities.calculatePinPosition(pin_no = 10, isLabel = False, pins_per_side = [10]) == (2, 10)
+    assert UIUtilities.calculatePinPosition(pin_no = 10, isLabel = True, pins_per_side = [10]) == (1, 10)
+
+    with pytest.raises(Exception) as e_info:
+        UIUtilities.calculatePinPosition(pin_no = 11, isLabel = True, pins_per_side = [10])
+
+    assert e_info.value.args[0] == 'Pin structure does not fit pin number 11'
+                                        
     
