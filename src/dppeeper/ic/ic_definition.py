f48db5e81c3a894cdce2d08a5fa2df686b3b5992
Fabio Battaglia <tabaglio@posteo.net>
2024-08-12 10:35:01 +0200

    Add required definitions for registered PALs

--- src/dppeeper/ic/ic_definition.py
+++ src/dppeeper/ic/ic_definition.py
@@ -18,6 +18,9 @@ class ICDefinition:
     io_pins: list[int]
     o_pins: list[int]
     f_pins: list[int]
+    q_pins: list[int]
+    oe_l_pins: list[int]
+    oe_h_pins: list[int]
 
     adapter_hi_pins: list[int]
     hw_model: int
@@ -33,7 +36,7 @@ class ICDefinition:
         return remapped
 
     @staticmethod
-    def _build_pin_names(zif_map: list[int], in_pins: list[int], io_pins: list[int], o_pins: list[int], clk_pins: list[int]) -> list[str]:
+    def _build_pin_names(zif_map: list[int], in_pins: list[int], io_pins: list[int], o_pins: list[int], clk_pins: list[int], q_pins: list[int], oe_l_pins: list[int], oe_h_pins: list[int]) -> list[str]:
         pin_names: list[str] = [('P' if pin == 42 else ('G' if pin == 21 else '')) for pin in zif_map]
 
         for pin in in_pins:
@@ -49,6 +52,15 @@ class ICDefinition:
             if len(pin_names[pin-1]) > 0:
                 pin_names[pin-1] = pin_names[pin-1] + '/CLK'
 
+        for pin in q_pins:
+            pin_names[pin-1] = f'Q{pin}'
+        
+        for pin in oe_h_pins:
+            pin_names[pin-1] = 'OE'
+        
+        for pin in oe_l_pins:
+            pin_names[pin-1] = '/OE'
+
         return pin_names
 
     def __init__(self,
@@ -60,6 +72,9 @@ class ICDefinition:
                  io_pins: list[int],
                  o_pins: list[int],
                  f_pins: list[int],
+                 q_pins: list[int],
+                 oe_l_pins: list[int],
+                 oe_h_pins: list[int],
                  adapter_hi_pins: list[int],
                  hw_model: int,
                  adapter_notes: str | None = None):
@@ -68,10 +83,20 @@ class ICDefinition:
         self.pins_per_side = pins_per_side
         self.zif_map = zif_map
         self.hw_model = hw_model
+
+        self.clk_pins = clk_pins
+        self.in_pins = in_pins
+        self.io_pins = io_pins
+        self.o_pins = o_pins
+        self.f_pins = f_pins
+        self.q_pins = q_pins
+        self.oe_h_pins = oe_h_pins
+        self.oe_l_pins = oe_l_pins
+
         self.adapter_notes = adapter_notes
         self.adapter_hi_pins = adapter_hi_pins
 
-        self.pin_names = self._build_pin_names(zif_map, in_pins, io_pins, o_pins, clk_pins)
+        self.pin_names = self._build_pin_names(zif_map, in_pins, io_pins, o_pins, clk_pins, q_pins, oe_l_pins, oe_h_pins)
 
         # Check the package
         tot_pins: int = sum(self.pins_per_side)
@@ -79,10 +104,4 @@ class ICDefinition:
             raise ValueError(f'Number of pins in name list {len(self.pin_names)} does not match pins in package ({tot_pins})')
         if len(self.pins_per_side) not in self._SUPPORTED_NUM_SIDES:
             raise ValueError(f'Number of sides {len(self.pins_per_side)} is not supported.')
-
-        self.clk_pins = clk_pins
-        self.in_pins = in_pins
-        self.io_pins = io_pins
-        self.o_pins = o_pins
-        self.f_pins = f_pins
 
